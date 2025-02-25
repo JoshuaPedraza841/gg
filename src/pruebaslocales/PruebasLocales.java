f388055c127372c64053ace02fcba6e9e8590473
joshua <joshua.pedraza@educa.madrid.org>
2025-02-25 19:27:05 +0100

    segundo commit

--- src/pruebaslocales/PruebasLocales.java
+++ src/pruebaslocales/PruebasLocales.java
@@ -5,6 +5,7 @@ public class PruebasLocales {
 
     public static void main(String[] args) {
         System.out.println("Aprendiendo GIT con el chino Nibi");
+        clase1.pintarMensaje("Nomura cabron");
     }
     
 }
