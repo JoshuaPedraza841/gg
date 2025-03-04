0dd75da2ed13a1180f954ce4521da527a1ac8654
JoshuaPedraza841 <145151195+JoshuaPedraza841@users.noreply.github.com>
2025-03-04 19:51:36 +0100

    Update PruebasLocales.java
    puto

--- src/pruebaslocales/PruebasLocales.java
+++ src/pruebaslocales/PruebasLocales.java
@@ -6,6 +6,7 @@ public class PruebasLocales {
     public static void main(String[] args) {
         System.out.println("Aprendiendo GIT con el chino Nibi");
         clase1.pintarMensaje("Nomura cabron");
+        //Comentario para mi
     }
     
 }
